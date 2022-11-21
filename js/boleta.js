d5afd867278d8079f25a1b362064993627ed91e5
Michael Valenzuela <66456332+Lekiam-VK@users.noreply.github.com>
2022-11-21 17:28:50 -0300

    Cambio en varios archivos
    1.- El archivo Boleta.html fue modificado para que tuviera lo necesario en el formulario.
    2.- El archivo boleta.css fue modificado para que el archivo boleta.html tuviera diseño.
    3.- Se creo el archivo boleta.js para añadir funcionalidad al archivo boleta.html

---
+++ js/boleta.js
@@ -0,0 +1,18 @@
+const dateInput = document.getElementById('date');
+
+// ✅ Using the visitor's timezone
+dateInput.value = formatDate();
+
+console.log(formatDate());
+
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function formatDate(date = new Date()) {
+    return [
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+    padTo2Digits(date.getDate()),
+    ].join('-');
+}
